0e81f80195d2cdb9b3a1af23f646b44b8eb175ec
karel.paulik <karelpaulik@email.cz>
2023-12-20 15:29:21 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div><q-icon name="thumb_up" size="md" color="teal" /></div>
+  </div>
+</template>
+
+<script setup>
+  import { useQuasar } from 'quasar'
+  const $q = useQuasar()
+  console.log($q.iconSet)
+</script>
